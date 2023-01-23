489118d91ebcec6b62653d056b4c95ea9d014469
gayatri0803 <85298488+gayatri0803@users.noreply.github.com>
2023-01-23 14:43:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
 // complete the given function
 
 function palindrome(str){
+	let reversed='';
+	for (char of str){
+		reversed=char+reversed;
+	}
+	if(str===reversed)
+	{
+		return true'
+	}
+	else
+	{
+		return false;
+	}
 
 }
 module.exports = palindrome
